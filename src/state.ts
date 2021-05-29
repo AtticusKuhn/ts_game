d6a09b8d0ebfd195d9ae95c6e8d4d780ccf0a709
eulerthedestroyer <eulerthedestroyer@gmail.com>
2021-05-29 12:47:50 -0700

    I'm done

--- src/state.ts
+++ src/state.ts
@@ -10,17 +10,9 @@ let state: state = {
   },
 };
 export const merge_state = (s: Partial<state>) => set_state({ ...state, ...s });
-export const set_state = (s: state) => {
-  state = s;
-  return state;
-};
+export const set_state = (s: state) => (state = s);
 export const get_state = () => state;
-export const set_player_position = (pt: point): state => {
-  state.player_position = pt;
-  return state;
-};
-
-export const move_player = (pt: point): state => {
-  state.player_position = add_points(state.player_position, pt);
-  return state;
-};
+export const set_player_position = (pt: point): state =>
+  Object.assign(state, { player_position: pt });
+export const move_player = (pt: point): state =>
+  set_player_position(add_points(state.player_position, pt));
